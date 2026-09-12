77258700e0e2bc931f897abd3df43e451cacfbe3
abdullahbenamer <benamer@gmail.com>
2026-09-12 12:23:29 +0200

    fix Bootstrap RTL CSS

--- app/Views/header.php
+++ app/Views/header.php
@@ -81,9 +81,25 @@ $direction = Language::direction();
     </title>
 
     <!-- Bootstrap -->
-    <link
+    <!-- <link
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
-        rel="stylesheet">
+        rel="stylesheet"> -->
+
+<?php if ($current_language === 'ar'): ?>
+
+<link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.rtl.min.css"
+    rel="stylesheet">
+
+<?php else: ?>
+
+<link
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+    rel="stylesheet">
+
+<?php endif; ?>
+
+
     <!-- Select2 -->
     <link
         href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css"
